75ad4b70816c38b8b4eb4ea32eb7100d52164619
CrappyDill <mddillon47@gmail.com>
2021-06-18 15:40:50 -0700

    Full game cycle, need to add more conditions.

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,9 +1,10 @@
 "use strict";
 exports.__esModule = true;
+var prompt = require('prompt-sync')();
 var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-// const player;
-// const dealer;
+var player = [];
+var dealer = [];
 function deckOfCards(suits, values) {
     var cards = [];
     for (var _i = 0, suits_1 = suits; _i < suits_1.length; _i++) {
@@ -14,11 +15,64 @@ function deckOfCards(suits, values) {
             cards.push(card);
         }
     }
+    getRandCard(cards);
     return cards;
 }
 var cards = deckOfCards(suits, values);
 function getRandCard(cards) {
     var randCard = cards[Math.floor(Math.random() * cards.length)];
-    console.log(randCard);
+    return randCard;
 }
-getRandCard(cards);
+function givePlayerCards() {
+    for (var i = 0; i <= 1; i++) {
+        player.push(getRandCard(cards));
+    }
+    console.log("Players Hand: [" + player[0]["value"] + " of " + player[0]["suit"] + "][" + player[1]["value"] + " of " + player[1]["suit"] + "]");
+}
+function giveDealerCards() {
+    for (var i = 0; i <= 1; i++) {
+        dealer.push(getRandCard(cards));
+    }
+    console.log("Dealers Hand: [" + dealer[0]["value"] + " of " + dealer[0]["suit"] + "][HIDDEN CARD]");
+}
+// 
+function hit() {
+    return getRandCard(cards);
+}
+var playersHand = givePlayerCards();
+var dealersHand = giveDealerCards();
+var playerDecision = prompt('Hit, Double or Stay');
+if (playerDecision === 'Hit' || 'Double') {
+    var hitCard = hit();
+    player.push(hitCard);
+    console.log("Players Hand: [" + player[0]["value"] + " of " + player[0]["suit"] + "][" + player[1]["value"] + " of " + player[1]["suit"] + "][" + player[2]["value"] + " of " + player[2]["suit"] + "]");
+    var playerDecision2 = prompt('Hit, Stay or Bust');
+    if (playerDecision2 === "Bust") {
+        console.log("Too Many, you lose. Ctrl C to restart.");
+    }
+    if (playerDecision2 === 'Hit') {
+        var hitCard_1 = hit();
+        player.push(hitCard_1);
+        console.log("Players Hand: [" + player[0]["value"] + " of " + player[0]["suit"] + "][" + player[1]["value"] + " of " + player[1]["suit"] + "][" + player[2]["value"] + " of " + player[2]["suit"] + "][" + player[3]["value"] + " of " + player[3]["suit"] + "]");
+        var playerDecision3 = prompt('Stay or Bust');
+        if (playerDecision3 === "Bust") {
+            console.log("Too Many, you lose. Ctrl C to restart.");
+        }
+    }
+}
+console.log("Dealers Hand: [" + dealer[0]["value"] + " of " + dealer[0]["suit"] + "][" + dealer[1]["value"] + " of " + dealer[1]["suit"] + "]");
+var dealerDecision = prompt('CPU - Hit or Stay');
+if (dealerDecision === 'Hit') {
+    var hitCard = hit();
+    dealer.push(hitCard);
+    console.log("Dealers Hand: [" + dealer[0]["value"] + " of " + dealer[0]["suit"] + "][" + dealer[1]["value"] + " of " + dealer[1]["suit"] + "][" + dealer[2]["value"] + " of " + dealer[2]["suit"] + "]");
+    var dealerDecision2 = prompt('Hit, Stay or Bust');
+    if (dealerDecision2 === "Bust") {
+        console.log("Too Many for the dealer! You Win!");
+    }
+    if (dealerDecision2 === 'Hit') {
+        var hitCard_2 = hit();
+        player.push(hitCard_2);
+        console.log("Players Hand: [" + dealer[0]["value"] + " of " + dealer[0]["suit"] + "][" + dealer[1]["value"] + " of " + dealer[1]["suit"] + "][" + dealer[2]["value"] + " of " + dealer[2]["suit"] + "][" + dealer[3]["value"] + " of " + dealer[3]["suit"] + "]");
+    }
+}
--- blackjack/index.ts
+++ blackjack/index.ts
@@ -1,9 +1,9 @@
-
+const prompt = require('prompt-sync')();
 let suits: string[] = ["Spades", "Hearts", "Diamonds", "Clubs"]
 let values: string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let player: {}[] = [];
+let dealer: {}[] = [];
 
-// const player;
-// const dealer;
 
 function deckOfCards(suits: string[], values: string[]) {
     let cards: {}[] = []
@@ -13,19 +13,76 @@ function deckOfCards(suits: string[], values: string[]) {
             cards.push(card)
         }
     }
+    getRandCard(cards)
     return cards;
 }
 
-
 let cards = deckOfCards(suits, values);
-
 
 function getRandCard(cards) {
     let randCard = cards[Math.floor(Math.random() * cards.length)];
-    console.log(randCard)
+    return randCard
 }
 
+function givePlayerCards() {
+    for (let i = 0; i <= 1; i++) {
+        player.push(getRandCard(cards));
+    }
+    console.log(`Players Hand: [${player[0]["value"]} of ${player[0]["suit"]}][${player[1]["value"]} of ${player[1]["suit"]}]`)
+}
 
-getRandCard(cards)
+function giveDealerCards() {
+    for (let i = 0; i <= 1; i++) {
+        dealer.push(getRandCard(cards));
+    }
+    console.log(`Dealers Hand: [${dealer[0]["value"]} of ${dealer[0]["suit"]}][HIDDEN CARD]`)
+}
+// 
+function hit() {
+   return getRandCard(cards)
+}
+let playersHand = givePlayerCards()
+let dealersHand = giveDealerCards()
 
-export {}
+let playerDecision = prompt('Hit, Double or Stay');
+
+if (playerDecision === 'Hit' || 'Double') {
+    let hitCard = hit();
+    player.push(hitCard)
+    console.log(`Players Hand: [${player[0]["value"]} of ${player[0]["suit"]}][${player[1]["value"]} of ${player[1]["suit"]}][${player[2]["value"]} of ${player[2]["suit"]}]`);
+    
+    let playerDecision2 = prompt('Hit, Stay or Bust');
+    if (playerDecision2 === "Bust") {
+        console.log("Too Many, you lose. Ctrl C to restart.")
+    }
+    if (playerDecision2 === 'Hit') {
+        let hitCard = hit();
+        player.push(hitCard)
+        console.log(`Players Hand: [${player[0]["value"]} of ${player[0]["suit"]}][${player[1]["value"]} of ${player[1]["suit"]}][${player[2]["value"]} of ${player[2]["suit"]}][${player[3]["value"]} of ${player[3]["suit"]}]`)
+        let playerDecision3 = prompt('Stay or Bust');
+        if (playerDecision3 === "Bust") {
+        console.log("Too Many, you lose. Ctrl C to restart.")
+        }
+    }  
+} 
+
+console.log(`Dealers Hand: [${dealer[0]["value"]} of ${dealer[0]["suit"]}][${dealer[1]["value"]} of ${dealer[1]["suit"]}]`)
+let dealerDecision = prompt('CPU - Hit or Stay')
+
+if (dealerDecision === 'Hit') {
+    let hitCard = hit();
+    dealer.push(hitCard)
+    console.log(`Dealers Hand: [${dealer[0]["value"]} of ${dealer[0]["suit"]}][${dealer[1]["value"]} of ${dealer[1]["suit"]}][${dealer[2]["value"]} of ${dealer[2]["suit"]}]`)
+    let dealerDecision2 = prompt('Hit, Stay or Bust');
+    if (dealerDecision2 === "Bust") {
+        console.log("Too Many for the dealer! You Win!")
+    }
+    if (dealerDecision2 === 'Hit') {
+        let hitCard = hit();
+        player.push(hitCard)
+        console.log(`Players Hand: [${dealer[0]["value"]} of ${dealer[0]["suit"]}][${dealer[1]["value"]} of ${dealer[1]["suit"]}][${dealer[2]["value"]} of ${dealer[2]["suit"]}][${dealer[3]["value"]} of ${dealer[3]["suit"]}]`)
+    }
+
+}
+
+export { }
