a0e078358c89dd82d1bd78f90f574514c5d2da99
CrappyDill <mddillon47@gmail.com>
2021-06-17 18:18:59 -0700

    Update: BJ deck array of objects

--- blackjack/index.js
+++ blackjack/index.js
@@ -2,16 +2,20 @@ var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 // const player;
 // const dealer;
-var deckOfCards = function (suits, values) {
-    var deck = [];
+function deckOfCards(suits, values) {
+    var cards = [];
     for (var _i = 0, suits_1 = suits; _i < suits_1.length; _i++) {
         var suit = suits_1[_i];
         for (var _a = 0, values_1 = values; _a < values_1.length; _a++) {
             var value = values_1[_a];
-            var card = [suit, value];
-            deck.push(card);
+            var card = { suit: suit, value: value };
+            cards.push(card);
         }
-        console.log(deck);
     }
-};
+    return cards;
+}
 deckOfCards(suits, values);
+// const getRandCard = (cards) => {
+//     console.log(cards)
+// }
+// getRandCard()
--- blackjack/index.ts
+++ blackjack/index.ts
@@ -1,19 +1,26 @@
 
 const suits: string[] = ["Spades", "Hearts", "Diamonds", "Clubs"]
 const values: string[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-const card: [suit, value] = 
 // const player;
 // const dealer;
 
-const deckOfCards = (suits: string[], values: string[]) => {
-    let deck = [];
+function deckOfCards(suits: string[], values: string[]) {
+    let cards: {}[] = []
     for (let suit of suits) {
         for (let value of values) {
-            let card: [string, string] = [suit, value]
-            deck.push(card)
+            let card: {} = { suit, value }
+            cards.push(card)
         }
-       
     }
+    return cards;
 }
 
-deckOfCards(suits, values)
+
+
+deckOfCards(suits, values);
+
+// const getRandCard = (cards) => {
+//     console.log(cards)
+// }
+
+// getRandCard()
